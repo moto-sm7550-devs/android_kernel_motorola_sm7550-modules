49d1cb5613dba446ed7c6f1a52c559129191c393
shihl5@motorola.com <shihl5@motorola.com>
2023-10-12 19:40:25 +0800

    eqe: enable MOT_OIS_EARLY_UPGRADE_FW Feature[2/3]
    details:
    Download OIS FW during boot up stage
    
    Change-Id: I095539a66bf80028556f8cb2738640ae745d27b1
    Signed-off-by: shihl5@motorola.com <shihl5@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2766045
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Wang Liu <liuwang2@lenovo.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- config/eqe.mk
+++ config/eqe.mk
@@ -6,6 +6,8 @@ CONFIG_CCI_DEBUG_INTF := y
 ccflags-y += -DCONFIG_CCI_DEBUG_INTF=1
 endif
 CONFIG_MOT_OIS_DW9784_DRIVER := y
+CONFIG_MOT_OIS_EARLY_UPGRADE_FW := y
 
 # Flags to pass into C preprocessor
 ccflags-y += -DCONFIG_MOT_OIS_DW9784_DRIVER=1
+ccflags-y += -DCONFIG_MOT_OIS_EARLY_UPGRADE_FW=1
--- config/eqe_factory.mk
+++ config/eqe_factory.mk
@@ -3,6 +3,8 @@
 # Localized KCONFIG settings
 CONFIG_MOT_OIS_DW9784_DRIVER := y
 CONFIG_CCI_DEBUG_INTF := y
+CONFIG_MOT_OIS_EARLY_UPGRADE_FW := y
 # Flags to pass into C preprocessor
 ccflags-y += -DCONFIG_MOT_OIS_DW9784_DRIVER=1
 ccflags-y += -DCONFIG_CCI_DEBUG_INTF=1
+ccflags-y += -DCONFIG_MOT_OIS_EARLY_UPGRADE_FW=1
