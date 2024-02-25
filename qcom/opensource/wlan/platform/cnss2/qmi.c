c7a484d15ac1703fdc6fc76472b107a9f048e803
SGCMarkus <markusornik@gmail.com>
2024-02-25 10:50:47 +0100

    qcom: wlan: cnss2: Add void argument to old-style zero prototype functions
    Change-Id: I961ffad6d08944f175195d6ca1924fcf9c6a2b5e

--- qcom/opensource/wlan/platform/cnss2/qmi.c
+++ qcom/opensource/wlan/platform/cnss2/qmi.c
@@ -166,7 +166,7 @@ static int num_of_products(moto_product *list)
 	return num;
 }
 
-static int get_moto_device()
+static int get_moto_device(void)
 {
         char *bootdevice = NULL;
         int rc = 0;
@@ -180,7 +180,7 @@ static int get_moto_device()
         }
 }
 
-static int get_moto_radio()
+static int get_moto_radio(void)
 {
         char *radiodevice = NULL;
         int rc = 0;
